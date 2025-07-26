10e5eb4868e97737487c584362578770daffb47e
kwanwoo <kwanwoo0929@gmail.com>
2025-07-27 01:03:02 +0900

    07/27 fix Indicator Bug

--- app/src/main/java/com/example/mokathon/OnboardingActivity.kt
+++ app/src/main/java/com/example/mokathon/OnboardingActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mokathon
 
 import android.os.Bundle
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
@@ -21,24 +20,5 @@ class OnboardingActivity : AppCompatActivity() {
 
         // ViewPager2 <-> TabLayout 연결
         TabLayoutMediator(tabLayout, viewPager) { _, _ -> }.attach()
-
-        /*
-        // 🔥 핵심! tabView의 사이즈를 강제로 정사각형으로 설정해서 원형 drawable이 눌리지 않게 함
-        tabLayout.post {
-            for (i in 0 until tabLayout.tabCount) {
-                val tabView = (tabLayout.getChildAt(0) as ViewGroup).getChildAt(i)
-                val params = tabView.layoutParams as ViewGroup.MarginLayoutParams
-                params.width = dpToPx(8)   // 원하는 원 사이즈
-                params.height = dpToPx(8)
-                tabView.layoutParams = params
-            }
-        }
-        */
-    }
-
-    // dp -> px 변환 함수
-    private fun dpToPx(dp: Int): Int {
-        val density = resources.displayMetrics.density
-        return (dp * density).toInt()
     }
 }
