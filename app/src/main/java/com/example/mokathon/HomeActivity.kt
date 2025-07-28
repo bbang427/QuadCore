91530066957f51c941f42f0437420e1d106af08c
kwanwoo <kwanwoo0929@gmail.com>
2025-07-29 04:31:50 +0900

    07/29 Home Greeting complete!!

--- app/src/main/java/com/example/mokathon/HomeActivity.kt
+++ app/src/main/java/com/example/mokathon/HomeActivity.kt
@@ -1,21 +1,43 @@
 package com.example.mokathon
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.isAppearanceLightStatusBars = true
+
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Greeting
+        auth = Firebase.auth
+
+        val tvGreeting: TextView = findViewById(R.id.tv_greeting)
+
+        val user = auth.currentUser
+        val name = user?.displayName ?: user?.email?.substringBefore("@") ?: "사용자"
+
+        tvGreeting.text = "안녕하세요, $name 님"
     }
 }
 //안녕하세요 반가워요 잘있어요 다시만나요.
--- app/src/main/java/com/example/mokathon/SignInActivity.kt
+++ app/src/main/java/com/example/mokathon/SignInActivity.kt
@@ -6,13 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class sign_inPage : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.fragment_landing_page)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.landingPage)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
