7a032ba72ce1faab89d0488c36ccfa5ceaa1f22b
kwanwoo <kwanwoo0929@gmail.com>
2025-08-05 01:42:09 +0900

    08/05 Home UI Fix and MinorBugFix

--- app/src/main/java/com/example/mokathon/HomeFragment.kt
+++ app/src/main/java/com/example/mokathon/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mokathon
 
 import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
@@ -33,28 +34,32 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         // 2. 클릭 리스너 설정
         clFirst.setOnClickListener {
-            Toast.makeText(requireContext(), "통화 녹음 업로드 클릭됨", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(requireContext(), "통화 녹음 업로드 클릭됨", Toast.LENGTH_SHORT).show()
 
             val intent = Intent(requireContext(), UploadActivity::class.java)
             startActivity(intent)
         }
         clSecond.setOnClickListener {
-            Toast.makeText(requireContext(), "계좌번호 조회 클릭됨", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(requireContext(), "계좌번호 조회 클릭됨", Toast.LENGTH_SHORT).show()
 
             val intent = Intent(requireContext(), SearchAccActivity::class.java)
             startActivity(intent)
         }
         clThird.setOnClickListener {
-            Toast.makeText(requireContext(), "전화번호 조회 클릭됨", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(requireContext(), "전화번호 조회 클릭됨", Toast.LENGTH_SHORT).show()
 
             val intent = Intent(requireContext(), SearchNumActivity::class.java)
             startActivity(intent)
         }
         clFourth.setOnClickListener {
-            Toast.makeText(requireContext(), "의심 번호 제보 클릭됨", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(requireContext(), "의심 번호 제보 클릭됨", Toast.LENGTH_SHORT).show()
 
             val intent = Intent(requireContext(), ReportActivity::class.java)
             startActivity(intent)
         }
+
+        //3. 네이버뉴스 바로가기 밑줄표시
+        val tv = view.findViewById<TextView>(R.id.tv_news_link)
+        tv.paintFlags = tv.paintFlags or Paint.UNDERLINE_TEXT_FLAG
     }
 }
--- app/src/main/java/com/example/mokathon/SearchNumActivity.kt
+++ app/src/main/java/com/example/mokathon/SearchNumActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchNumActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.isAppearanceLightStatusBars = true
         setContentView(R.layout.activity_search_num)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
