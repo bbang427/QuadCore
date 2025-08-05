63d172bbb97b45aa4b491c604d6af122b8e21b23
kwanwoo <kwanwoo0929@gmail.com>
2025-08-06 03:30:07 +0900

    08/06 Upload service and minorfix

--- app/src/main/java/com/example/mokathon/SearchNumActivity.kt
+++ app/src/main/java/com/example/mokathon/SearchNumActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mokathon
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +21,11 @@ class SearchNumActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backBtn = findViewById<ImageView>(R.id.btn_back)
+        backBtn.setOnClickListener {
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/mokathon/UploadActivity.kt
+++ app/src/main/java/com/example/mokathon/UploadActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mokathon
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +22,11 @@ class UploadActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backBtn = findViewById<ImageView>(R.id.btn_back)
+        backBtn.setOnClickListener {
+            finish()
         }
     }
 }
